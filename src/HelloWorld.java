a54b09712edc77a8f41027e7c0ca6427553edfe8
Nour Solaiman <112700614+NourSolaiman@users.noreply.github.com>
2023-03-08 21:48:23 +0100

    Update HelloWorld.java
    We will see

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String args [])
     {
-        System.out.println("This is my hello program");
+        System.out.println("This is my program");
         System.out.println(100+2+55);
     }
 
