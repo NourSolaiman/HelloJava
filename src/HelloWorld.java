b859103c279c61e33b18b41c7184a09edab84839
mac <mac@macs-air.mynet>
2023-03-08 21:44:57 +0100

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String args [])
+    {
+        System.out.println("This is my hello program");
+        System.out.println(100+2+55);
+    }
+
+}
