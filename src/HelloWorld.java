8edd7ec97fe097c89e292d73299cd3d437ad6e0b
mac <solaiman.nour@gmail.com>
2023-03-08 21:54:04 +0100

    A new adding

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
     public static void main(String args [])
     {
         System.out.println("This is my program");
-        System.out.println(100+2+55);
+        System.out.println(100+2+55+700);
+        System.out.println("Which call HELLO WORLD");
     }
 
 }
